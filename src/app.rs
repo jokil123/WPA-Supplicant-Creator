e3abcec91f836d0c2a17d92f093f36349fb9c289
jo_kil <jo.kil.plays@gmail.com>
2023-03-26 20:55:47 +0200

    error handling

--- src/app.rs
+++ src/app.rs
@@ -18,37 +18,69 @@ impl eframe::App for Ui {
             ui.heading("WPA Supplicant Creator");
 
             if ui.button("Detect").clicked() {
-                self.config_data = get_config_data().unwrap();
+                match get_config_data() {
+                    Ok(d) => self.config_data = d,
+                    Err(e) => native_dialog::MessageDialog::new()
+                        .set_type(native_dialog::MessageType::Error)
+                        .set_title("Error")
+                        .set_text(format!("Failed to Detect Informtation!\nError:\n{}", e).as_str())
+                        .show_alert()
+                        .unwrap(),
+                };
             };
 
             egui::Grid::new("input").num_columns(2).show(ui, |ui| {
-                let label = ui.label("Country");
+                let label_country = ui.label("Country");
                 ui.text_edit_singleline(&mut self.config_data.country)
-                    .labelled_by(label.id);
+                    .labelled_by(label_country.id);
                 ui.end_row();
 
-                let label = ui.label("Wifi SSID");
+                let label_ssid = ui.label("Wifi SSID");
                 ui.text_edit_singleline(&mut self.config_data.network.ssid)
-                    .labelled_by(label.id);
+                    .labelled_by(label_ssid.id);
                 ui.end_row();
 
-                let label = ui.label("Wifi Password");
+                let label_password = ui.label("Wifi Password");
                 ui.add(
                     egui::TextEdit::singleline(&mut self.config_data.network.password)
                         .password(true),
-                );
+                )
+                .labelled_by(label_password.id);
                 ui.end_row();
 
-                let label = ui.label("Output File Path");
+                let label_output_path = ui.label("Output File Path");
                 let mut path_string_buf = self.config_data.path.to_string_lossy().to_string();
 
-                if ui.text_edit_singleline(&mut path_string_buf).changed() {
+                if ui
+                    .text_edit_singleline(&mut path_string_buf)
+                    .labelled_by(label_output_path.id)
+                    .changed()
+                {
                     self.config_data.path = PathBuf::from(path_string_buf);
                 }
             });
 
             if ui.button("Create Config File").clicked() {
-                create_config_file(&self.config_data).unwrap();
+                match create_config_file(&self.config_data) {
+                    Ok(_) => {
+                        native_dialog::MessageDialog::new()
+                            .set_type(native_dialog::MessageType::Info)
+                            .set_title("Success")
+                            .set_text("Config file created successfully!")
+                            .show_alert()
+                            .unwrap();
+                    }
+                    Err(e) => {
+                        native_dialog::MessageDialog::new()
+                            .set_type(native_dialog::MessageType::Error)
+                            .set_title("Error")
+                            .set_text(
+                                format!("Config file creation failed!\nError:\n{}", e).as_str(),
+                            )
+                            .show_alert()
+                            .unwrap();
+                    }
+                };
             };
         });
     }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use eframe::{egui, AppCreator};
-use wpa_supplicant_creator::{
-    app::Ui, create_config_file::create_config_file, get_config_data::get_config_data,
-};
+use eframe::egui;
+use wpa_supplicant_creator::app::Ui;
 
 fn main() {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(320.0, 180.0)),
+        centered: true,
         ..Default::default()
     };
 
